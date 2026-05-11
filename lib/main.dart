424543e33f59092b8f0f0e1e8a96208c3b3a2fb9
Kauã Rodrigo <kaua.rodrigo@souunit.com.br>
2026-05-11 19:02:23 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello'),
+          child: Text('Olá, Mundo!'),
         ),
       ),
     );
