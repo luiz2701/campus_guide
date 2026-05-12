3775bb84ab8929021b1a8d392b9747a0d54ae082
Kauã Rodrigo <kauarodrigo1@gmail.com>
2026-05-12 11:22:58 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-//teste
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
